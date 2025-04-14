a970d8bbbef4ec61e19d2e3ff7fe76acfcfb06c4
WolfHunter-HKIT <gytis.sakalys@stud.kitm.lt>
2025-04-14 14:30:38 +0300

    Added a search bar. Fixed minor issues with code appearance

--- api-dev/src/App.jsx
+++ api-dev/src/App.jsx
@@ -4,29 +4,29 @@ import CountryFetch from './services/countryFetch';
 import CountryFilters from './components/countryFilters';
 
 function App() {
-  const [showAllCountries, setShowAllCountries] = useState(true);
-  const [countries, setCountries] = useState([]);
+	const [showAllCountries, setShowAllCountries] = useState(true);
+	const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-      const data = await CountryFetch();
-      setCountries(data);
-    };
-    fetchCountries();
-  }, []);
+	useEffect(() => {
+		const fetchCountries = async () => {
+			const data = await CountryFetch();
+			setCountries(data);
+		};
+		fetchCountries();
+	}, []);
 
-  return (
-    <div className="App">
-      <CountryFilters showAllCountries={setShowAllCountries} />
-      {showAllCountries && (
-        <div className="countryCards">
-          {countries.map((country) => (
-            <CountryCard key={country.name.common} country={country} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className='App'>
+			<CountryFilters showAllCountries={setShowAllCountries} />
+			{showAllCountries && (
+				<div className='countryCards'>
+					{countries.map((country) => (
+						<CountryCard key={country.name.common} country={country} />
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- api-dev/src/components/countryFilters.jsx
+++ api-dev/src/components/countryFilters.jsx
@@ -3,52 +3,77 @@ import CountryFetch from '../services/countryFetch';
 import CountryCard from './countryCard';
 
 const CountryFilters = ({ showAllCountries }) => {
-  const [filter, setFilter] = useState('all');
-  const [countries, setCountries] = useState([]);
-  const [regions, setRegions] = useState([]);
+	const [filter, setFilter] = useState('all');
+	const [countries, setCountries] = useState([]);
+	const [regions, setRegions] = useState([]);
+	const [search, setSearch] = useState('');
 
-  // Fetches countries data when the component mounts
-  useEffect(() => {
-    CountryFetch()
-      .then((data) => {
-        setCountries(data);
-        const uniqueRegions = [...new Set(data.map((country) => country.region))];
-        setRegions(['all', ...uniqueRegions]);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+	// Fetches countries data when the component mounts
+	useEffect(() => {
+		CountryFetch()
+			.then((data) => {
+				setCountries(data);
+				const uniqueRegions = [...new Set(data.map((country) => country.region))];
+				setRegions(['all', ...uniqueRegions]);
+			})
+			.catch((error) => console.error(error));
+	}, []);
 
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
-    showAllCountries(false);
-  };
+	// Handles region filters on click
+	const handleFilterChange = (e) => {
+		setFilter(e.target.value);
+		showAllCountries(false);
+	};
 
-  return (
-    <div>
-      <div className="countryFilters">
-        {regions.map((region) => (
-          <button
-            key={region}
-            className="filterButton"
-            value={region}
-            onClick={handleFilterChange}
-            style={{
-              backgroundColor: filter === region ? '#ccc' : '#f0f0f0',
-            }}
-          >
-            {region.toUpperCase()}
-          </button>
-        ))}
-      </div>
-      <div className="countryCards">
-        {countries
-          .filter((country) => (filter === 'all' ? true : country.region === filter))
-          .map((country) => (
-            <CountryCard key={country.name.common} country={country} />
-          ))}
-      </div>
-    </div>
-  );
+	// Handles specific searches on change
+	const handleSearchChange = async (e) => {
+		setSearch(e.target.value);
+		if (e.target.value.length > 2) {
+			showAllCountries(false);
+		}
+	};
+
+	return (
+		<div>
+			<div className='countryFilters'>
+				<div>
+					{regions.map((region) => (
+						<button
+							key={region}
+							className='filterButton'
+							value={region}
+							onClick={handleFilterChange}
+							style={{
+								backgroundColor: filter === region ? '#ccc' : '#f0f0f0',
+							}}
+						>
+							{region.toUpperCase()}
+						</button>
+					))}
+				</div>
+				<input
+					type='text'
+					name='searchBar'
+					id='searchBar'
+					onChange={handleSearchChange}
+					value={search}
+					placeholder='Search here'
+				/>
+			</div>
+			<div className='countryCards'>
+				{countries
+					.filter((country) => (filter === 'all' ? true : country.region === filter))
+					.filter((country) =>
+						search.length < 3
+							? true
+							: country.name.common.toLowerCase().includes(search.toLowerCase())
+					)
+					.map((country) => (
+						<CountryCard key={country.name.common} country={country} />
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default CountryFilters;
